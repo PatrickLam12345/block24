d2e408f5ff0d4044b0472b6d1cbf4c41e1b118fb
Patrick Lam <patricklam12345@gmail.com>
2023-08-28 21:28:00 -0400

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,28 @@ import { useState } from 'react'
 function App() {
 
   const [puppies, setPuppies] = useState(puppyList);
+  const [pupId, setPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === pupId);
+  console.log(featuredPup)
   console.log(puppies)
 
   return (
     <>
       <div className="App">
-        { 
+        {pupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+        {
           puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
+            return <p onClick={() => { setPupId(puppy.id) }} key={puppy.id}>
+              {puppy.name}
+            </p>
           })
         }
       </div>
